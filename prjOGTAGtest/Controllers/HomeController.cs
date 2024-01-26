ca52cba1fc6a6af50b573720f3ab72cdff2e391b
語喬 郭 <kakuc0e0ig@gmail.com>
2024-01-26 14:03:02 +0800

    加入專案檔案。

---
+++ prjOGTAGtest/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjOGTAGtest.Models;
+using System.Diagnostics;
+
+namespace prjOGTAGtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GiftIndex()
+        {
+            CIndex cIndex = new CIndex()
+            {
+                商品名 = "咖啡",
+                賀卡圖 = "https://prjdbgamingformshow20231219140402.azurewebsites.net/images/shop/633234ed-a8d6-4cd0-80d6-07a5e4363d5b.jpg"
+            };
+            return View(cIndex);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ prjOGTAGtest/Models/CIndex.cs
@@ -0,0 +1,8 @@
+﻿namespace prjOGTAGtest.Models
+{
+    public class CIndex
+    {
+        public string 商品名 { get; set; }
+        public string 賀卡圖 { get; set; }
+    }
+}
